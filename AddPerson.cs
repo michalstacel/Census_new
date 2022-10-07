0e94ead2dc6275e36101a3a5d4c469c6bca98ee9
Michał Stącel <michalstacel@gmail.com>
2022-10-07 19:11:25 +0200

    Dodaj pliki projektów.

---
+++ AddPerson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace Census
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(int Id1, string Name1, string Surname1, int Age1)
+        {
+            Id = Id1;
+            Name = Name1;
+            Surname = Surname1;
+            Age = Age1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Census;
+using System.Xml.Linq;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+Console.WriteLine($"1. Add Person");
+Console.WriteLine($"2. Remove Person");
+Console.WriteLine($"3. Listing");
+
+int.TryParse(Console.ReadLine(), out int readline);
+
+var people = new List<Person> { };
+
+
+
+switch (readline)
+{
+    case 1:
+
+        Person p = CreatePerson();
+
+        people.Add(p);
+        break;
+
+    case 2:
+
+        break;
+
+    case 3:
+
+        break;
+
+    default:
+        Console.WriteLine("Wrong number");
+        break;
+
+}
+foreach (var item in people)
+{
+    Console.WriteLine(item.Name);
+}
+
+
+static Person CreatePerson()
+{
+    Console.WriteLine("Id ?");
+    int Id = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Name ?");
+    string Name = Console.ReadLine();
+    Console.WriteLine("Surname ?");
+    string Surname = Console.ReadLine();
+    Console.WriteLine("Age?");
+    int Age = Convert.ToInt32(Console.ReadLine());
+
+    Person p = new Person(Id, Name, Surname, Age);
+    return p;
+}
